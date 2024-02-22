78bebfd54dfa37572af872768346e4a2dfbb6a11
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-02-22 22:21:28 +0100

    File savings tests now use tmp_path

--- tests/test_filedata_manager.py
+++ tests/test_filedata_manager.py
@@ -3,31 +3,23 @@ import os
 from carlogger.filedata_manager import JSONFiledataManager
 
 
-def test_saves_data_as_json(mock_component):
+def test_saves_data_as_json(mock_component, tmp_path):
     filename = mock_component.name + ".json"
     saver = JSONFiledataManager()
-    saver.save_file(mock_component, "./")
+    saver.save_file(mock_component, f"{tmp_path}/")
 
     files: list[str] = os.listdir(".")
 
     assert filename in files
 
 
-def test_loads_data_as_json(mock_component):
-    filename = mock_component.name + ".json"
-    saver = JSONFiledataManager()
-    data = saver.load_file(f"./{filename}")
-
-    assert data is not None
-
-
-def test_save_and_load_json(mock_component):
+def test_save_and_load_json(mock_component, tmp_path):
     data: dict = mock_component.to_json()
 
     filename = mock_component.name + ".json"
     saver = JSONFiledataManager()
-    saver.save_file(mock_component, "./")
+    saver.save_file(mock_component, f"{tmp_path}/")
 
-    loaded_data = saver.load_file(f"./{filename}")
+    loaded_data = saver.load_file(f"{tmp_path}/{filename}")
 
     assert data == loaded_data
