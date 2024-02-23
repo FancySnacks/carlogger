04e949f5dda2741553d9e886b5f732d5722de72f
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-02-23 21:12:06 +0100

    Created new ArgParser class

--- src/carlogger/__main__.py
+++ src/carlogger/__main__.py
@@ -1,9 +1,14 @@
 """Entrance point of this project"""
 
+from carlogger.argparser import ArgParser
 
-def main() -> int:
+
+def main(argv: list[str] = None) -> int:
+    parser = ArgParser()
+    parsed_args: dict = parser.parse_args(argv)
+
     return 0
 
 
 if __name__ == "__main__":
-    raise(SystemExit(main()))
+    raise SystemExit(main())
---
+++ src/carlogger/argparser.py
@@ -0,0 +1,20 @@
+"""Argument parser for CLI input"""
+
+import argparse
+
+
+class ArgParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(prog="Car Logger",
+                                              usage="carlogger [-args]",
+                                              description="description",
+                                              epilog="epilog")
+
+    def setup_args(self):
+        self.parser.add_argument()
+        # argument for creating collections
+        # argument for creating components
+        # argument for creating entries
+
+    def parse_args(self, args: list[str]) -> dict:
+        return self.parser.parse_args(args).__dict__
