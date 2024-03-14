11daa0ba9e9c0886b333db5c935258fd919a9420
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-03-14 21:34:22 +0100

    Added tests for ArgParser

---
+++ tests/test_argparser.py
@@ -0,0 +1,11 @@
+from carlogger.argparser import ArgParser
+
+
+def test_parses_args_to_parse():
+    args_to_parse = ['--gui']
+    parser = ArgParser()
+    parser.setup_args()
+
+    parsed_args = parser.parse_args(args_to_parse)
+
+    assert len(parsed_args.keys()) > 0
