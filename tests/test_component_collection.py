4e245e962590bb5edce56ed9e222d2b0cacfaadc
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-05-06 21:49:48 +0200

    Fixed invalid function name

--- tests/test_component_collection.py
+++ tests/test_component_collection.py
@@ -6,6 +6,6 @@ def test_get_function_returns_entry_logs(mock_log_entry, mock_component_collecti
     c.create_entry(mock_log_entry)
 
     mock_component_collection.components.append(c)
-    logs = mock_component_collection.get_all_log_entries()
+    logs = mock_component_collection.get_all_entry_logs()
 
     assert len(logs) > 0
