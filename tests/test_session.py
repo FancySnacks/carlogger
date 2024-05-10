bdab5605134e6d4b163a6e786fbc4fe40ebb2bcf
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-05-10 21:25:41 +0200

    Added test cases for --clear flag

--- tests/test_session.py
+++ tests/test_session.py
@@ -101,3 +101,53 @@ def test_entry_is_removed_by_unique_id(directory_manager, mock_car_directory, tm
     next_count = len(session.get_car_by_name(car_name).get_component_by_name('SparkPlug').log_entries)
 
     assert prev_count != next_count
+
+
+def test_car_children_are_cleared(directory_manager, mock_car_directory, tmp_path, mock_log_entry):
+    car_name = mock_car_directory['car_dir'].name
+    directory_manager.car_save_dir = tmp_path
+
+    session = AppSession(directory_manager)
+    session.load_car_dir(car_name)
+
+    session.selected_car.create_collection('Test')
+    session.add_new_component(car_name, 'Test', 'SparkPlug')
+    session.add_new_entry(car_name, 'Test', 'SparkPlug', mock_log_entry)
+
+    session.delete_car_children(session.selected_car)
+
+    assert sum([len(session.selected_car.collections), len(session.selected_car.get_all_entry_logs())]) == 0
+
+
+def test_collection_children_are_cleared(directory_manager, mock_car_directory, tmp_path, mock_log_entry):
+    car_name = mock_car_directory['car_dir'].name
+    directory_manager.car_save_dir = tmp_path
+
+    session = AppSession(directory_manager)
+    session.load_car_dir(car_name)
+
+    parent_coll = session.selected_car.create_collection('Engine')
+    parent_coll.create_component('SparkPlug')
+    session.add_new_nested_collection(car_name, 'Turbocharger', 'Engine')
+    session.add_new_entry(car_name, 'Engine', 'SparkPlug', mock_log_entry)
+
+    session.delete_collection_children(car_name, parent_coll)
+
+    assert len(parent_coll.children) == 0
+
+
+def test_component_children_are_cleared(directory_manager, mock_car_directory, tmp_path, mock_log_entry):
+    car_name = mock_car_directory['car_dir'].name
+    directory_manager.car_save_dir = tmp_path
+
+    session = AppSession(directory_manager)
+    session.load_car_dir(car_name)
+
+    parent_coll = session.selected_car.create_collection('Engine')
+    comp = parent_coll.create_component('SparkPlug')
+    session.add_new_entry(car_name, 'Engine', 'SparkPlug', mock_log_entry)
+    session.add_new_entry(car_name, 'Engine', 'SparkPlug', mock_log_entry)
+
+    session.delete_component_children(comp, session.selected_car)
+
+    assert len(comp.log_entries) == 0
