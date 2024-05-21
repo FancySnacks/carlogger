22bb5ffab3c0cc730c4eded84039764b2c043933
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-05-21 20:17:30 +0200

    Added basic navigation widget

--- src/carlogger/gui/root_window.py
+++ src/carlogger/gui/root_window.py
@@ -3,6 +3,8 @@ from customtkinter import CTk
 from carlogger.gui.w_frame import Frame
 from carlogger.gui.c_carlist import CarList
 from carlogger.gui.w_carframe import CarFrame
+from carlogger.gui.w_navigation import NavigationBar
+
 
 class RootWindow(CTk):
     def __init__(self):
@@ -15,10 +17,18 @@ class RootWindow(CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        self.main_frame = Frame(master=self, width=700, height=1000, corner_radius=0, fg_color="transparent")
+        self.main_frame = Frame(master=self, corner_radius=0, fg_color="transparent")
         self.main_frame.grid(row=0, column=0, sticky="nsew")
 
-        self.car_list = CarList(CarFrame(self.main_frame, width=700, height=1000))
+        self.main_frame.grid_columnconfigure(0, weight=1)
+
+        self.navigation = NavigationBar(master=self.main_frame)
+        self.navigation.grid(row=0, column=0, sticky='ew')
+
+        self.car_frame = CarFrame(self.main_frame)
+        self.car_frame.grid(row=1, column=0, sticky='nsew')
+
+        self.car_list = CarList(self.car_frame)
 
     def start_mainloop(self):
         self.mainloop()
