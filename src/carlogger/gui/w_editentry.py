69c4a8a11c82b815a74f8f25c250bf0dd8cef853
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-06-05 17:02:27 +0200

    Added custom property edit menu to Entry Edit Window

--- src/carlogger/gui/w_editentry.py
+++ src/carlogger/gui/w_editentry.py
@@ -1,4 +1,4 @@
-from customtkinter import CTk, CTkFrame, CTkLabel, CTkButton, CTkEntry, CTkTextbox, CTkOptionMenu
+from customtkinter import CTk, CTkFrame, CTkLabel, CTkButton, CTkEntry, CTkTextbox, CTkOptionMenu, CTkScrollableFrame
 
 from tkinter import END
 
@@ -35,13 +35,19 @@ class EditEntryPopup:
         
         # ===== Edit Values ===== #
 
-        self.edit_container = CTkFrame(self.main_frame, fg_color='transparent')
-        self.edit_container.pack(anchor='center', fill='both', pady=25, padx=15)
+        self.edit_main_frame = CTkFrame(self.main_frame, fg_color='transparent')
+        self.edit_main_frame.pack(anchor='center', fill='both', pady=25, padx=15)
+
+        self.edit_left_frame = CTkFrame(self.edit_main_frame)
+        self.edit_left_frame.grid(row=0, column=0, sticky='nsew', pady=10)
+
+        self.edit_mid_frame = CTkFrame(self.edit_main_frame)
+        self.edit_mid_frame.grid(row=0, column=1, sticky='nsew', pady=10)
 
         # ===== Date ===== #
 
-        self.date_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.date_frame.grid(row=0, column=0, sticky='w', pady=5)
+        self.date_frame = CTkFrame(self.edit_left_frame, fg_color='transparent')
+        self.date_frame.grid(row=0, column=0, sticky='w', pady=10)
 
         self.date_label = CTkLabel(self.date_frame, text="Date", font=('Lato', 20))
         self.date_label.grid(row=0, column=0, sticky='w')
@@ -55,8 +61,8 @@ class EditEntryPopup:
 
         # ===== Desc ===== #
 
-        self.desc_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.desc_frame.grid(row=1, column=0, sticky='w', pady=5)
+        self.desc_frame = CTkFrame(self.edit_left_frame, fg_color='transparent')
+        self.desc_frame.grid(row=1, column=0, sticky='w', pady=10)
 
         self.desc_label = CTkLabel(self.desc_frame, text="Description", font=('Lato', 20))
         self.desc_label.grid(row=0, column=0, sticky='w')
@@ -67,8 +73,8 @@ class EditEntryPopup:
 
         # ===== Component ===== #
 
-        self.component_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.component_frame.grid(row=2, column=0, sticky='w', pady=5)
+        self.component_frame = CTkFrame(self.edit_left_frame, fg_color='transparent')
+        self.component_frame.grid(row=2, column=0, sticky='w', pady=10)
 
         self.component_label = CTkLabel(self.component_frame, text="Parent Component", font=('Lato', 20))
         self.component_label.grid(row=0, column=0, sticky='w')
@@ -79,10 +85,10 @@ class EditEntryPopup:
         self.component_menu.set(self.item_ref.component.name)
         self.component_menu.grid(row=1, column=0, sticky='w')
 
-        # ===== Component ===== #
+        # ===== Category ===== #
 
-        self.category_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.category_frame.grid(row=3, column=0, sticky='w', pady=5)
+        self.category_frame = CTkFrame(self.edit_left_frame, fg_color='transparent')
+        self.category_frame.grid(row=3, column=0, sticky='w', pady=10)
 
         self.category_label = CTkLabel(self.category_frame, text="Category", font=('Lato', 20))
         self.category_label.grid(row=0, column=0, sticky='w')
@@ -91,11 +97,11 @@ class EditEntryPopup:
                                            values=[e for e in EntryCategory])
         self.category_menu.set(self.item_ref.category)
         self.category_menu.grid(row=1, column=0, sticky='w')
-        
+
         # ===== Mileage ===== #
 
-        self.mileage_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.mileage_frame.grid(row=4, column=0, sticky='w', pady=5)
+        self.mileage_frame = CTkFrame(self.edit_left_frame, fg_color='transparent')
+        self.mileage_frame.grid(row=4, column=0, sticky='w', pady=10)
 
         self.mileage_label = CTkLabel(self.mileage_frame, text="Mileage", font=('Lato', 20))
         self.mileage_label.grid(row=0, column=0, sticky='w')
@@ -107,17 +113,81 @@ class EditEntryPopup:
         self.mileage_unit_label = CTkLabel(self.mileage_frame, text="km", font=('Lato', 20))
         self.mileage_unit_label.grid(row=1, column=1, sticky='w', padx=10)
 
-        # ===== Mileage ===== #
+        # ===== Custom Info ===== #
 
-        self.mileage_frame = CTkFrame(self.edit_container, fg_color='transparent')
-        self.mileage_frame.grid(row=4, column=0, sticky='w', pady=5)
+        self.custom_frame = CTkScrollableFrame(self.edit_mid_frame, fg_color='transparent', width=550, height=800)
+        self.custom_frame.grid(row=0, column=0, sticky='w', pady=10, padx=100)
 
-        self.mileage_label = CTkLabel(self.mileage_frame, text="Mileage", font=('Lato', 20))
-        self.mileage_label.grid(row=0, column=0, sticky='w')
+        self.custom_label = CTkLabel(self.custom_frame, text="Custom Properties", font=('Lato', 20))
+        self.custom_label.grid(row=0, column=0, sticky='w')
 
-        self.mileage_entry = CTkEntry(self.mileage_frame, font=('Lato', 20), placeholder_text='Enter mileage (km)')
-        self.mileage_entry.insert(0, self.item_ref.mileage)
-        self.mileage_entry.grid(row=1, column=0, sticky='w')
+        self.add_property_button = CTkButton(self.custom_frame,
+                                             text="+",
+                                             font=('Lato', 20),
+                                             fg_color='green',
+                                             width=35,
+                                             corner_radius=0,
+                                             command=self.add_new_property)
+        self.add_property_button.grid(row=0, column=1, sticky='w', padx=15, pady=5)
 
-        self.mileage_unit_label = CTkLabel(self.mileage_frame, text="km", font=('Lato', 20))
-        self.mileage_unit_label.grid(row=1, column=1, sticky='w', padx=10)
+        self.property_container = PropertyContainer(self.custom_frame,
+                                                    self.root,
+                                                    self.item_ref,
+                                                    width=250)
+        self.property_container.grid(row=1, column=0, columnspan=5, sticky='w')
+
+        self.property_container.create_properties()
+
+    def add_new_property(self):
+        self.property_container.add_property()
+
+
+class PropertyContainer(CTkFrame):
+    def __init__(self, master, root: CTk, item_ref, **values):
+        super().__init__(master, **values)
+        self.master = master
+        self.root = root
+        self.item_ref = item_ref
+
+        self.properties: dict[str, ...] = item_ref.custom_info
+        self.property_widgets: list[PropertyItem] = []
+
+    def add_property(self, name: str = 'New Property', value='Enter value'):
+        new_item = PropertyItem(self, self.root, name, value, len(self.properties))
+        self.property_widgets.append(new_item)
+
+    def create_properties(self):
+        for p in self.properties.items():
+            self.add_property(p[0], p[1])
+
+
+class PropertyItem:
+    def __init__(self, master, root: CTk, property_name: str, property_value, index: int):
+        self.master = master
+        self.root = root
+
+        self.index: int = index
+
+        self.property_name: str = property_name
+        self.property_value = property_value
+
+        # ===== Widget ===== #
+
+        self.property_frame = CTkFrame(self.master, fg_color='gray', width=400)
+        self.property_frame.pack(fill='x', anchor='w', padx=2, pady=2)
+
+        self.property_name_label = CTkEntry(self.property_frame, font=('Lato', 17), width=200)
+        self.property_name_label.insert(0, self.property_name.capitalize())
+        self.property_name_label.grid(row=0, column=0, sticky='w', padx=3, pady=2)
+
+        self.separator = CTkLabel(self.property_frame,
+                                  text='',
+                                  bg_color='lightgray',
+                                  height=20,
+                                  width=3,
+                                  font=('Arial', 2))
+        self.separator.grid(row=0, column=1, sticky='w', padx=10)
+
+        self.property_value_label = CTkEntry(self.property_frame, font=('Lato', 17), width=300)
+        self.property_value_label.insert(0, self.property_value)
+        self.property_value_label.grid(row=0, column=2, sticky='w', padx=3, pady=2)
