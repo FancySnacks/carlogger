3260955df0a361b8bc4d9ae07cf104a3f1daf788
FancySnacks <adrian.urbaniak1336@gmail.com>
2024-07-29 18:20:37 +0200

    Changed search bar

--- src/carlogger/gui/w_searchbar.py
+++ src/carlogger/gui/w_searchbar.py
@@ -1,4 +1,4 @@
-from customtkinter import CTkEntry, CTkFrame, CTkLabel, StringVar
+from customtkinter import CTkButton, CTkFrame
 
 from carlogger.gui.const_gui import search_icon_mini
 
@@ -8,27 +8,13 @@ class Searchbar:
         self.master = master
         self.root = root
 
-        self.search_var = StringVar(value='')
-        self.search_var.trace_add('write', self.track_changes)
-
         self.main_frame = CTkFrame(master=self.master, fg_color='transparent')
-        self.main_frame.pack(expand=False, side='right', padx=10, pady=5)
+        self.main_frame.pack(padx=5, pady=5)
 
-        self.search_img = CTkLabel(master=self.main_frame, image=search_icon_mini, text='')
-        self.search_img.pack(expand=False, side='left', padx=10)
-
-        self.search_bar = CTkEntry(master=self.main_frame,
-                                   placeholder_text="Search",
-                                   textvariable=self.search_var,
-                                   width=200,
-                                   height=30,
-                                   font=('Lato', 17))
-        self.search_bar.pack(expand=True, fill='x')
-
-    def track_changes(self, *args):
-        self.search_var.get()
-
-
-class SearchResult:
-    def __init__(self):
-        pass
+        self.button = CTkButton(master=self.main_frame,
+                                text='Search',
+                                width=100,
+                                height=30,
+                                image=search_icon_mini,
+                                font=('Lato', 17))
+        self.button.pack(padx=5, pady=5)
